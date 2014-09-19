d6c9b85201e1acc33b11a30fd4a508f20c2cd3bf
Casey Marshall <cmars@cmarstech.com>
2014-09-18 23:31:05 -0500

    Support replying to query, split out types into package.
    Can actually connect to it with a mongo client now.

---
+++ gonzo/types.go
@@ -0,0 +1,267 @@
+package gonzo
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"sync"
+
+	"gopkg.in/mgo.v2/bson"
+)
+
+var requestIDLock sync.Mutex
+var requestID = int32(0)
+
+func newRequestID() int32 {
+	requestIDLock.Lock()
+	defer requestIDLock.Unlock()
+	requestID++
+	return requestID
+}
+
+type OpCode int32
+
+var (
+	OpReply       = OpCode(1)
+	OpMsg         = OpCode(1000)
+	OpUpdate      = OpCode(2001)
+	OpInsert      = OpCode(2002)
+	OpQuery       = OpCode(2004)
+	OpGetMore     = OpCode(2005)
+	OpDelete      = OpCode(2006)
+	OpKillCursors = OpCode(2007)
+)
+
+type Header struct {
+	Length     int32
+	RequestID  int32
+	ResponseTo int32
+	OpCode     OpCode
+	Contents   []byte
+}
+
+func readInt32(b []byte) (int32, []byte, bool) {
+	if len(b) >= 4 {
+		return int32(binary.LittleEndian.Uint32(b[:4])), b[4:], true
+	}
+	return 0, nil, false
+}
+
+func readCstring(b []byte) (string, []byte, bool) {
+	for i := 0; i < len(b); i++ {
+		if b[i] == 0 {
+			return string(b[:i]), b[i+1:], true
+		}
+	}
+	return "", nil, false
+}
+
+func readBsonDoc(b []byte, out interface{}) ([]byte, error) {
+	l, _, ok := readInt32(b)
+	if !ok {
+		return nil, errTruncMsg
+	}
+	if int(l) > len(b) {
+		l = int32(len(b))
+	}
+	if err := bson.Unmarshal(b[:l], out); err != nil {
+		return nil, err
+	}
+	return b[l:], nil
+}
+
+func writeInt32(w io.Writer, b []byte, v int32) error {
+	binary.LittleEndian.PutUint32(b, uint32(v))
+	_, err := w.Write(b[:4])
+	return err
+}
+
+func writeInt64(w io.Writer, b []byte, v int64) error {
+	binary.LittleEndian.PutUint64(b, uint64(v))
+	_, err := w.Write(b[:8])
+	return err
+}
+
+var errTruncMsg = fmt.Errorf("truncated message")
+
+func (h *Header) Read(r io.Reader) error {
+	buf := make([]byte, 4)
+	_, err := r.Read(buf)
+	if err != nil {
+		return err
+	}
+	var ok bool
+	h.Length, _, ok = readInt32(buf)
+	if !ok {
+		return errTruncMsg
+	}
+
+	buf = make([]byte, h.Length-4)
+	_, err = r.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	if h.RequestID, buf, ok = readInt32(buf); !ok {
+		return errTruncMsg
+	}
+	if h.ResponseTo, buf, ok = readInt32(buf); !ok {
+		return errTruncMsg
+	}
+
+	opCode, buf, ok := readInt32(buf)
+	if !ok {
+		return errTruncMsg
+	} else {
+		h.OpCode = OpCode(opCode)
+	}
+
+	h.Contents = buf
+	return nil
+}
+
+func (h *Header) Write(w io.Writer) error {
+	b := make([]byte, 4)
+	var err error
+	if err = writeInt32(w, b, h.Length); err != nil {
+		return err
+	}
+	if err = writeInt32(w, b, h.RequestID); err != nil {
+		return err
+	}
+	if err = writeInt32(w, b, h.ResponseTo); err != nil {
+		return err
+	}
+	if err = writeInt32(w, b, int32(h.OpCode)); err != nil {
+		return err
+	}
+	_, err = w.Write(h.Contents)
+	return err
+}
+
+type QueryFlags int32
+
+const (
+	QueryFlagTailableCursor  = 1 << 1
+	QueryFlagSlaveOk         = 1 << 2
+	QueryFlagOplogReplay     = 1 << 3
+	QueryFlagNoCursorTimeout = 1 << 4
+	QueryFlagAwaitData       = 1 << 5
+	QueryFlagExhaust         = 1 << 6
+	QueryFlagPartial         = 1 << 7
+)
+
+type OpQueryMsg struct {
+	*Header
+
+	// Bit vector of query options.
+	Flags QueryFlags
+
+	// "dbname.collectionname"
+	FullCollectionName string
+
+	// number of documents to skip
+	NumberToSkip int32
+
+	// number of documents to return in the first OP_REPLY batch
+	NumberToReturn int32
+
+	// query object.  See below for details.
+	QueryDoc map[string]interface{}
+
+	// Optional. Selector indicating the fields to return.
+	ReturnFieldsSelector map[string]int
+}
+
+func NewOpQueryMsg(h *Header) (*OpQueryMsg, error) {
+	m := &OpQueryMsg{Header: h}
+	b := h.Contents
+
+	flags, b, ok := readInt32(b)
+	if !ok {
+		return nil, errTruncMsg
+	} else {
+		m.Flags = QueryFlags(flags)
+	}
+
+	if m.FullCollectionName, b, ok = readCstring(b); !ok {
+		return nil, errTruncMsg
+	}
+	if m.NumberToSkip, b, ok = readInt32(b); !ok {
+		return nil, errTruncMsg
+	}
+	if m.NumberToReturn, b, ok = readInt32(b); !ok {
+		return nil, errTruncMsg
+	}
+
+	var err error
+	m.QueryDoc = make(map[string]interface{})
+	if b, err = readBsonDoc(b, &m.QueryDoc); err != nil {
+		return nil, err
+	}
+	if len(b) > 0 {
+		m.ReturnFieldsSelector = make(map[string]int)
+		if b, err = readBsonDoc(b, &m.ReturnFieldsSelector); err != nil {
+			return nil, err
+		}
+	}
+	return m, nil
+}
+
+type OpReplyMsg struct {
+	*Header
+
+	// bit vector - see details below
+	ResponseFlags int32
+
+	// cursor id if client needs to do get more's
+	CursorID int64
+
+	// where in the cursor this reply is starting
+	StartingFrom int32
+
+	// number of documents in the reply
+	NumberReturned int32
+
+	// documents
+	Docs []interface{}
+
+	responseTo int32
+}
+
+func NewOpReplyMsg(responseTo int32, docs ...interface{}) *OpReplyMsg {
+	return &OpReplyMsg{
+		Header: &Header{
+			RequestID:  newRequestID(),
+			ResponseTo: responseTo,
+			OpCode:     OpReply,
+		},
+		NumberReturned: int32(len(docs)),
+		Docs:           docs,
+	}
+}
+
+func (m *OpReplyMsg) Write(w io.Writer) error {
+	b := make([]byte, 8)
+	var out bytes.Buffer
+
+	writeInt32(&out, b, m.ResponseFlags)
+	writeInt64(&out, b, m.CursorID)
+	writeInt32(&out, b, m.StartingFrom)
+	writeInt32(&out, b, m.NumberReturned)
+
+	for _, doc := range m.Docs {
+		b, err := bson.Marshal(doc)
+		if err != nil {
+			return err
+		}
+		_, err = out.Write(b)
+		if err != nil {
+			return err
+		}
+	}
+	m.Header.Contents = out.Bytes()
+	m.Header.Length = int32(out.Len() + 16)
+	return m.Header.Write(w)
+}
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
-	"io"
 	"log"
 	"net"
 
+	"github.com/cmars/gonzodb/gonzo"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -29,179 +28,57 @@ func main() {
 	}
 }
 
-type OpCode int32
-
-var (
-	OpReply       = OpCode(1)
-	OpMsg         = OpCode(1000)
-	OpUpdate      = OpCode(2001)
-	OpInsert      = OpCode(2002)
-	OpQuery       = OpCode(2004)
-	OpGetMore     = OpCode(2005)
-	OpDelete      = OpCode(2006)
-	OpKillCursors = OpCode(2007)
-)
-
-type msg struct {
-	Length     int32
-	RequestID  int32
-	ResponseTo int32
-	OpCode     OpCode
-	Contents   []byte
-}
-
-func readInt32(b []byte) (int32, []byte, bool) {
-	if len(b) >= 4 {
-		return int32(binary.LittleEndian.Uint32(b[:4])), b[4:], true
-	}
-	return 0, nil, false
-}
-
-func readCstring(b []byte) (string, []byte, bool) {
-	for i := 0; i < len(b); i++ {
-		if b[i] == 0 {
-			return string(b[:i]), b[i+1:], true
-		}
-	}
-	return "", nil, false
-}
-
-func readBsonDoc(b []byte, out interface{}) ([]byte, error) {
-	l, _, ok := readInt32(b)
-	if !ok {
-		return nil, errTruncMsg
-	}
-	if int(l) > len(b) {
-		l = int32(len(b))
-	}
-	if err := bson.Unmarshal(b[:l], out); err != nil {
-		return nil, err
-	}
-	return b[l:], nil
-}
-
-var errTruncMsg = fmt.Errorf("truncated message")
-
-func (m *msg) Read(r io.Reader) error {
-	buf := make([]byte, 4)
-	_, err := r.Read(buf)
-	if err != nil {
-		return err
-	}
-	var ok bool
-	m.Length, _, ok = readInt32(buf)
-	if !ok {
-		return errTruncMsg
-	}
-
-	buf = make([]byte, m.Length-4)
-	_, err = r.Read(buf)
-	if err != nil {
-		return err
-	}
-
-	if m.RequestID, buf, ok = readInt32(buf); !ok {
-		return errTruncMsg
-	}
-	if m.ResponseTo, buf, ok = readInt32(buf); !ok {
-		return errTruncMsg
-	}
-
-	opCode, buf, ok := readInt32(buf)
-	if !ok {
-		return errTruncMsg
-	} else {
-		m.OpCode = OpCode(opCode)
-	}
-
-	m.Contents = buf
-	return nil
-}
-
-type queryFlags int32
-
-const (
-	queryFlagTailableCursor  = 1 << 1
-	queryFlagSlaveOk         = 1 << 2
-	queryFlagOplogReplay     = 1 << 3
-	queryFlagNoCursorTimeout = 1 << 4
-	queryFlagAwaitData       = 1 << 5
-	queryFlagExhaust         = 1 << 6
-	queryFlagPartial         = 1 << 7
-)
-
-type queryMsg struct {
-	// Bit vector of query options.
-	flags queryFlags
-	// "dbname.collectionname"
-	fullCollectionName string
-	// number of documents to skip
-	numberToSkip int32
-	// number of documents to return in the first OP_REPLY batch
-	numberToReturn int32
-	// query object.  See below for details.
-	query map[string]interface{}
-	// Optional. Selector indicating the fields to return.
-	returnFieldsSelector map[string]int
-}
-
-func (m *queryMsg) SetBytes(b []byte) error {
-	flags, b, ok := readInt32(b)
-	if !ok {
-		return errTruncMsg
-	} else {
-		m.flags = queryFlags(flags)
-	}
-
-	if m.fullCollectionName, b, ok = readCstring(b); !ok {
-		return errTruncMsg
-	}
-	if m.numberToSkip, b, ok = readInt32(b); !ok {
-		return errTruncMsg
-	}
-	if m.numberToReturn, b, ok = readInt32(b); !ok {
-		return errTruncMsg
-	}
-
-	var err error
-	m.query = make(map[string]interface{})
-	if b, err = readBsonDoc(b, &m.query); err != nil {
-		return err
-	}
-	if len(b) > 0 {
-		m.returnFieldsSelector = make(map[string]int)
-		if b, err = readBsonDoc(b, &m.returnFieldsSelector); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func handle(c net.Conn) {
 	defer c.Close()
-	msg := &msg{}
-	err := msg.Read(c)
-	if err != nil {
-		log.Printf("header read: %v", err)
-		return
-	}
-	log.Printf("got: %v", msg)
-	switch msg.OpCode {
-	//case OpReply:
-	//case OpMsg:
-	//case OpUpdate:
-	//case OpInsert:
-	case OpQuery:
-		qmsg := &queryMsg{}
-		if err = qmsg.SetBytes(msg.Contents); err != nil {
-			log.Printf("query error: %v", err)
-		} else {
-			log.Printf("query: %v", qmsg)
+	for {
+		h := &gonzo.Header{}
+		err := h.Read(c)
+		if err != nil {
+			log.Printf("header read: %v", err)
+			return
 		}
-	//case OpGetMore:
-	//case OpDelete:
-	//case OpKillCursors:
-	default:
-		log.Printf("unsupported op code: %v", msg)
+		switch h.OpCode {
+		//case OpReply:
+		//case OpMsg:
+		//case OpUpdate:
+		//case OpInsert:
+		case gonzo.OpQuery:
+			query, err := gonzo.NewOpQueryMsg(h)
+			if err != nil {
+				log.Println("query error:", err)
+				return
+			}
+			if query.FullCollectionName == "admin.$cmd" {
+				handleAdminCommand(c, query)
+				continue
+			}
+			log.Println("unsupported query:", query)
+		//case OpGetMore:
+		//case OpDelete:
+		//case OpKillCursors:
+		default:
+			log.Println("unsupported op code:", h.OpCode)
+		}
+	}
+}
+
+func handleAdminCommand(c net.Conn, query *gonzo.OpQueryMsg) {
+	var err error
+
+	if v, ok := query.QueryDoc["whatsmyuri"]; ok && v == 1 {
+		resp := gonzo.NewOpReplyMsg(query.RequestID, bson.D{{"you", c.RemoteAddr().String()}})
+		err = resp.Write(c)
+	} else {
+		errmsg := fmt.Sprintf("unsupported admin command: %v", query)
+		log.Println(errmsg)
+		resp := gonzo.NewOpReplyMsg(query.RequestID, bson.D{
+			{"errmsg", errmsg},
+			{"ok", 0},
+		})
+		err = resp.Write(c)
+	}
+
+	if err != nil {
+		log.Println(err)
 	}
 }
